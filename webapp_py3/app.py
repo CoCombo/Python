9340dfa180e5456f2bf6a8022e79d2a7649a5eef
CoCombo <419178356@qq.com>
2016-05-15 22:49:23 +0800

    update

--- webapp_py3/app.py
+++ webapp_py3/app.py
@@ -9,4 +9,12 @@ def index(request):
 
 @asyncio.coroutine
 def init(loop):
-	app = web.Application
+	app = web.Application(loop = loop)
+	app.router.add_route('GET', '/', index)
+	srv = yield fromn loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+	logging.info('server started at http://127.0.0.1:9000...')
+	return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forver()
