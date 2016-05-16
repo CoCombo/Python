1faa4a6ec7db7047b4e6b487cd321bdf92ca53fb
CoCombo <419178356@qq.com>
2016-05-16 13:23:30 +0800

    update

--- webapp_py3/orm.py
+++ webapp_py3/orm.py
@@ -17,3 +17,71 @@ def create_pool(loop, **kw):
 		minsize = kw.get('minsize', 1),
 		loop = loop
 		)
+
+@asyncio.coroutine
+def select(sql, args, size = None):
+	log(sql, args)
+	global __pool
+	with (yield from __pool) as conn:
+		cur = yield from conn.cursor(aiomysql.DictCursor)
+	 	yield from cur.execuate(sql.replace('?', '%s'), args or ())
+		if size:
+			rs = yield from cur.fetchmany(size)
+		else:
+			yield from cur.fetchall()
+		yield from cur.close()
+		logging.info('rows returned: %s' % len(rs))
+		return rs
+
+@asyncio.coroutine
+def execute(sql, args):
+	log(sql):
+	with (yiel from __pool) as conn:
+		try:
+			cur = yield from conn.cursor()
+			yield from cur.execuate(sql.replace('?', '%s'), args)
+			affected = cur.rowcount
+			yield from cur.close()
+		except BaseException as e:
+			raise
+		return affected
+
+class Model(dict, metaclass = ModelMetaclass):
+	def __init__(self, **kw):
+		super(Model, self).__init__(**kw)
+	
+	def __getattr__(self, key):	
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+	
+	def __setattr__(self, key, value):
+		self[key] = value
+	
+	def getValue(self, key):
+		return getattr(self, key, None)
+	
+	def getValueOrDefault(self, key):
+		value = getattr(self, key, None)
+		if value is None:
+			field = self.__mappings__[key]
+			if field.default is not None:
+				value = field.default() if callable(field.default) else field.default
+				logging.debug('using default value for %s: %s' % (key, str(value)))
+				setattr(self, key, value)
+		return value
+
+class Field(object):
+	def __init__(self, name, column_type, primary_key, default):
+		self.name = name
+		self.column_type = column_type
+		self.primary_key = primary_key
+		self.default = default
+	
+	def __str__(self):
+		return '<%s, %s: s>' % (self.__class__.__name__, self.column_type, self.name)
+
+class StringField(Field):
+	def __init(self, name = None, primary_key =  False, default = None, ddl = 'varchar(100)'):
+		super().__init__(name, ddl, primary_key, default)
